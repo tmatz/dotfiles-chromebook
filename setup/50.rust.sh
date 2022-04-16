d43262e368d6fbb81d178ef687c02414e30ce557
MATSUDA Takashi <tmatz.dev@gmail.com>
2022-04-17 02:43:23 +0900

    setup/50.rust.sh

---
+++ setup/50.rust.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# see http://asdf-vm.com/guide/getting-started.html
+asdf plugin add rust
+asdf install rust latest
+asdf global rust latest
+rustup component add rls rust-analysis rust-src
