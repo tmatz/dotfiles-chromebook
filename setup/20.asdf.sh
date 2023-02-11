7c1bf9c104f4667a2123dc5371b3b1c5955f52bc
MATSUDA Takashi <tmatz.dev@gmail.com>
2023-02-11 13:16:55 +0900

    setup/20.asdf.sh

--- setup/20.asdf.sh
+++ setup/20.asdf.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # see http://asdf-vm.com/guide/getting-started.html
-sudo apt install curl git
-test -d ~/.asdf || git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
+sudo apt install -y curl git
+if [ ! -d ~/.asdf ]; then
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
+fi
+. ../rc/50.asdf.sh
+asdf update -all
